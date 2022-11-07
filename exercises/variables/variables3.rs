d6b3ba74f54890fbd907d99a4cce211ee3d519b1
Jianhong <tinyouse@gmail.com>
2022-11-07 16:08:09 +0800

    第二次

--- exercises/variables/variables3.rs
+++ exercises/variables/variables3.rs
@@ -1,9 +1,10 @@
 // variables3.rs
 // Execute `rustlings hint variables3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let x: i32;
+    //let x = 10i32 / 10_i32
+    let x: i32 = 10;
     println!("Number {}", x);
 }
